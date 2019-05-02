228d6d61da46dae04eb7608a115d10905d62f976
KOREAN139 <korean139@gmail.com>
2019-05-02 12:11:42 -0700

    <feat> rough sketch for syntax tree structure
    it's only rough sketch, need to refine structure more explicitly

--- src/globals.h
+++ src/globals.h
@@ -57,6 +57,48 @@ extern FILE *code;
 /* source line number for listing */
 extern int lineno;
 
+/* specify node's kind */
+typedef enum {
+        StmtK, ExprK, DeclK
+} NodeKind;
+/* specify statement's kind */
+typedef enum {
+        IfK, ReturnK, WhileK
+} StmtKind;
+/* specify expression's kind */
+typedef enum {
+        OpK, ConstK, IdK
+} ExprKind;
+/* specify declaration's kind*/
+typedef enum {
+        VarK, FunK
+} DeclKind;
+/* specify expression's type */
+typedef enum {
+        Void, Integer
+} ExprType;
+
+#define MAXCHILDREN 3
+/* structure of node in syntax tree */
+typedef struct _TreeNode {
+        struct _TreeNode *child[MAXCHILDREN];
+        struct _TreeNode *sibling;
+        int lineno;
+        NodeKind nodekind;
+        union {
+                StmtKind stmt;
+                ExprKind expr;
+                DeclKind decl;
+        } kind;
+        union {
+                TokenType op;
+                int val;
+                char *name;
+        } attr;
+        int val;
+        ExprType type;
+} TreeNode;
+
 /* 
  * TraceScan = TRUE causes token information to be
  * printed to the listing file as each token is
